d8cea6651980fd395086014f8163d93b5cb340b5
Michael Nascimento <michael.nsc@outlook.com>
2024-08-17 23:33:03 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import assert from "node:assert";
 import { it } from "node:test";
-import { add } from "./index.js";
+import { add, subtract } from "./index.js";
 
 it("should sum 1 + 1 equals 2", () => {
   assert.equal(add(1, 1), 2);
