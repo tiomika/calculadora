3151152ba927bf95084d45d1550d512efabaab8c
Michael Nascimento <michael.nsc@outlook.com>
2024-08-17 23:32:35 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ import { it } from "node:test";
 import { add } from "./index.js";
 
 it("should sum 1 + 1 equals 2", () => {
-  assert.equal(add(1, 1), 1);
+  assert.equal(add(1, 1), 2);
 });
 
 it("should subtract 1 - 1 equals 0", () => {
