f3323c66be61d384b83787958f89243de248c41d
Michael Nascimento <michael.nsc@outlook.com>
2024-08-17 23:19:49 -0300

    create fn

--- index.test.js
+++ index.test.js
@@ -1,13 +1,15 @@
+import assert from "node:assert";
+import { it } from "node:test";
+import { add } from "./";
 
-import assert from 'node:assert'
-import { it } from 'node:test'
-import { add } from './'
+it("should return true", (t) => {
+  assert.equal(1, 1);
+});
 
+it("should sum 1 + 1 equals 2", () => {
+  assert.equal(add(1, 1), 1);
+});
 
-it('should return true', (t) => {
-  assert.equal(1, 1)
-})
-
-it('should sum 1 + 1 equals 2', ()=> {
-  assert.equal(add(1,1), 1)
-})
+it("should subtract 1 - 1 equals 0", () => {
+  assert.equal(add(1, 1), 0);
+});
